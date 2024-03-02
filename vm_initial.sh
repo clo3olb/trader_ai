7f755ce080c82eebe398fa920e6d6a5c91889237
clo3olb <clo3olb@gmail.com>
2024-03-02 10:45:11 +0800

    Update Python environment and install required packages

--- vm_initial.sh
+++ vm_initial.sh
@@ -1,31 +1,19 @@
-sudo apt-get update
 
-# Installing git and configure
-sudo apt-get install git
+
 git config --global user.name "Joseph Kim"
 git config --global user.email "clo3olb@gmail.com"
 git clone https://github.com/clo3olb/trader_ai.git
 
-
-# go into the directory(~/trader_ai) and run the following commands
-
-# Python Setting
-# Note that python is already installed
+# Python is already installed in the image
+# PIP is already installed in the image
 # install venv
+sudo apt-get update
 sudo apt install python3.11-venv
 python3 -m venv trader_ai
-
-# To activate python venv
 source trader_ai/bin/activate
+
+# install packages
 pip install -r requirements.txt
-
-# For GPU like NVIDIA Tesla V100, You need to install GPU driver
-# You may refer to https://github.com/GoogleCloudPlatform/compute-gpu-installation/tree/main/linux
-sudo python3 install_gpu_driver.py
-
-# To deactivate python venv
-deactivate
-
 
 
 
