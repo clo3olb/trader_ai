e151a7544eb5bd85e437a2707de308accaecb5ea
clo3olb <clo3olb@gmail.com>
2024-03-12 15:12:59 +0800

    Better graph readability and minor formatting

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -16,25 +16,31 @@ def adjust_learning_rate(optimizer, scheduler, epoch, args, printout=True):
             10: 5e-7, 15: 1e-7, 20: 5e-8
         }
     elif args.lradj == 'type3':
-        lr_adjust = {epoch: args.learning_rate if epoch < 3 else args.learning_rate * (0.9 ** ((epoch - 3) // 1))}
+        lr_adjust = {epoch: args.learning_rate if epoch <
+                     3 else args.learning_rate * (0.9 ** ((epoch - 3) // 1))}
     elif args.lradj == 'constant':
         lr_adjust = {epoch: args.learning_rate}
     elif args.lradj == '3':
-        lr_adjust = {epoch: args.learning_rate if epoch < 10 else args.learning_rate*0.1}
+        lr_adjust = {epoch: args.learning_rate if epoch <
+                     10 else args.learning_rate*0.1}
     elif args.lradj == '4':
-        lr_adjust = {epoch: args.learning_rate if epoch < 15 else args.learning_rate*0.1}
+        lr_adjust = {epoch: args.learning_rate if epoch <
+                     15 else args.learning_rate*0.1}
     elif args.lradj == '5':
-        lr_adjust = {epoch: args.learning_rate if epoch < 25 else args.learning_rate*0.1}
+        lr_adjust = {epoch: args.learning_rate if epoch <
+                     25 else args.learning_rate*0.1}
     elif args.lradj == '6':
-        lr_adjust = {epoch: args.learning_rate if epoch < 5 else args.learning_rate*0.1}  
+        lr_adjust = {epoch: args.learning_rate if epoch <
+                     5 else args.learning_rate*0.1}
     elif args.lradj == 'TST':
         lr_adjust = {epoch: scheduler.get_last_lr()[0]}
-    
+
     if epoch in lr_adjust.keys():
         lr = lr_adjust[epoch]
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr
-        if printout: print('Updating learning rate to {}'.format(lr))
+        if printout:
+            print('Updating learning rate to {}'.format(lr))
 
 
 class EarlyStopping:
@@ -54,7 +60,8 @@ class EarlyStopping:
             self.save_checkpoint(val_loss, model, path)
         elif score < self.best_score + self.delta:
             self.counter += 1
-            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            print(
+                f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
@@ -64,7 +71,8 @@ class EarlyStopping:
 
     def save_checkpoint(self, val_loss, model, path):
         if self.verbose:
-            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+            print(
+                f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
         torch.save(model.state_dict(), path + '/' + 'checkpoint.pth')
         self.val_loss_min = val_loss
 
@@ -93,23 +101,26 @@ def visual(true, preds=None, name='./pic/test.pdf'):
     Results visualization
     """
     plt.figure()
-    plt.plot(true, label='GroundTruth', linewidth=2)
+    plt.plot(true, label='GroundTruth', linewidth=1)
     if preds is not None:
-        plt.plot(preds, label='Prediction', linewidth=2)
+        plt.plot(preds, label='Prediction', linewidth=1)
     plt.legend()
     plt.savefig(name, bbox_inches='tight')
 
-def test_params_flop(model,x_shape):
+
+def test_params_flop(model, x_shape):
     """
     If you want to thest former's flop, you need to give default value to inputs in model.forward(), the following code can only pass one argument to forward()
     """
     model_params = 0
     for parameter in model.parameters():
         model_params += parameter.numel()
-        print('INFO: Trainable parameter count: {:.2f}M'.format(model_params / 1000000.0))
-    from ptflops import get_model_complexity_info    
+        print('INFO: Trainable parameter count: {:.2f}M'.format(
+            model_params / 1000000.0))
+    from ptflops import get_model_complexity_info
     with torch.cuda.device(0):
-        macs, params = get_model_complexity_info(model.cuda(), x_shape, as_strings=True, print_per_layer_stat=True)
+        macs, params = get_model_complexity_info(
+            model.cuda(), x_shape, as_strings=True, print_per_layer_stat=True)
         # print('Flops:' + flops)
         # print('Params:' + params)
         print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
