67b6a0fb43a709e8d36c125c10a74868e6d875fb
Joseph Kim <clo3olb@gmail.com>
2024-03-12 08:41:24 +0000

    Remove unnecessary print statements and save correlation separately

--- src/exp/exp_main.py
+++ src/exp/exp_main.py
@@ -341,17 +341,9 @@ class Exp_Main(Exp_Basic):
         f.write('\n')
         f.close()
 
-        print('Shapes of the variables:')
-        print('mae:', np.shape(mae))
-        print('mse:', np.shape(mse))
-        print('rmse:', np.shape(rmse))
-        print('mape:', np.shape(mape))
-        print('mspe:', np.shape(mspe))
-        print('rse:', np.shape(rse))
-        print('corr:', np.shape(corr))
-
         np.save(folder_path + 'metrics.npy',
-                np.array([mae, mse, rmse, mape, mspe, rse, corr]))
+                np.array([mae, mse, rmse, mape, mspe, rse]))
+        np.save(folder_path + 'corr.npy', corr)
         np.save(folder_path + 'pred.npy', preds)
         np.save(folder_path + 'true.npy', trues)
         np.save(folder_path + 'x.npy', inputx)
