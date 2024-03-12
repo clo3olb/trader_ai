34fd83e5dc11b583440973bb3566628de89d1b7c
Joseph Kim <clo3olb@gmail.com>
2024-03-12 08:22:16 +0000

    Refactor run_experiment.py to use createSetting function

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -1,5 +1,5 @@
 import torch
-from args import parseArgs, saveArgs, getArgsPath
+from args import createSetting, parseArgs, saveArgs, getArgsPath
 from exp.exp_main import Exp_Main
 import random
 import numpy as np
@@ -74,22 +74,7 @@ if args.is_training:
         torch.cuda.empty_cache()
 else:
     ii = 0
-    setting = '{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_fc{}_eb{}_dt{}_{}_{}'.format(args.model_id,
-                                                                                                  args.model,
-                                                                                                  args.data,
-                                                                                                  args.features,
-                                                                                                  args.seq_len,
-                                                                                                  args.label_len,
-                                                                                                  args.pred_len,
-                                                                                                  args.d_model,
-                                                                                                  args.n_heads,
-                                                                                                  args.e_layers,
-                                                                                                  args.d_layers,
-                                                                                                  args.d_ff,
-                                                                                                  args.factor,
-                                                                                                  args.embed,
-                                                                                                  args.distil,
-                                                                                                  args.des, ii)
+    setting = createSetting(args, ii)
 
     exp = Exp(args)  # set experiments
     print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
