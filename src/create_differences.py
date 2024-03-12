5c07799e7b46f432e2e68e440dae735ffc0756f7
Joseph Kim <clo3olb@gmail.com>
2024-03-12 12:16:58 +0000

    Add functions to calculate differences and differences in percentage

---
+++ src/create_differences.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+def calculate_differences(csv_file):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(csv_file)
+
+    # extract first column
+    first_column = df.iloc[:, 0]
+
+    # remove first column
+    df = df.iloc[:, 1:]
+
+    # Calculate the differences
+    differences = df.diff()
+
+    # Add the first column back to the DataFrame
+    differences.insert(0, 'date', first_column)
+
+    # remove first row
+    differences = differences.iloc[1:, :]
+
+    # Return the DataFrame with differences
+    return differences
+
+def calculate_differences_in_percentage(csv_file):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(csv_file)
+
+    # extract first column
+    first_column = df.iloc[:, 0]
+
+    # remove first column
+    df = df.iloc[:, 1:]
+
+    # Calculate the differences
+    differences = df.pct_change()
+
+    # Add the first column back to the DataFrame
+    differences.insert(0, 'date', first_column)
+
+    # remove first row
+    differences = differences.iloc[1:, :]
+
+    # Return the DataFrame with differences
+    return differences
+
+# Example usage
+csv_file = './src/dataset/AAPL.csv'
+differences_df = calculate_differences(csv_file)
+differences_df_percentage = calculate_differences_in_percentage(csv_file)
+
+# Save the differences DataFrame to a new CSV file
+differences_df.to_csv('./src/dataset/AAPL_diff.csv', index=False)
+differences_df_percentage.to_csv('./src/dataset/AAPL_diff_pct.csv', index=False)
