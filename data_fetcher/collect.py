90d30ff27042c14cebbce73d5b4a4d26c3cad980
Joseph Kim <clo3olb@gmail.com>
2024-03-16 07:18:49 +0000

    Collect one month data

--- data_fetcher/collect.py
+++ data_fetcher/collect.py
@@ -1,17 +1,16 @@
 from dotenv import dotenv_values
 from market import getMonths, createMergedCSV
-import asyncio
 import os
 
 # Load the environment variables from the .env file
 env_vars = dotenv_values("local.env")
 
 
-async def main():
+def main():
     symbol = "AAPL"
     interval = "1min"
     start_date = "2022-01"
-    end_date = "2022-03"
+    end_date = "2022-01"
     api_key = env_vars['ALPHA_VANTAGE_API_KEY']
     result_file = "data_fetcher/merged_data.csv"
 
@@ -21,7 +20,7 @@ async def main():
     months = getMonths(start_date, end_date)
 
     for month in months:
-        await createMergedCSV(symbol, interval, month, api_key, result_file)
+        createMergedCSV(symbol, interval, month, api_key, result_file)
 
 
-asyncio.run(main())
+main()
