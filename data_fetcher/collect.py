f53a8258a1f16405df59910afd06cc5c4ba25f66
clo3olb <clo3olb@gmail.com>
2024-03-16 13:54:18 +0800

    refactor for better fetching

--- data_fetcher/collect.py
+++ data_fetcher/collect.py
@@ -1,10 +1,10 @@
 from dotenv import dotenv_values
 from market import getMonths, createMergedCSV
 import asyncio
+import os
 
 # Load the environment variables from the .env file
 env_vars = dotenv_values("local.env")
-api_key = env_vars['ALPHA_VANTAGE_API_KEY']
 
 
 async def main():
@@ -12,14 +12,16 @@ async def main():
     interval = "1min"
     start_date = "2022-01"
     end_date = "2022-03"
-    api_key = "YOUR_API_KEY"
-    result_file = "merged_data.csv"
+    api_key = env_vars['ALPHA_VANTAGE_API_KEY']
+    result_file = "data_fetcher/merged_data.csv"
+
+    if os.path.exists(result_file):
+        os.remove(result_file)
 
     months = getMonths(start_date, end_date)
 
-    tasks = [createMergedCSV(symbol, interval, month, api_key, result_file) for month in months]
-
-    await asyncio.gather(*tasks)
+    for month in months:
+        await createMergedCSV(symbol, interval, month, api_key, result_file)
 
 
 asyncio.run(main())
--- data_fetcher/market.py
+++ data_fetcher/market.py
@@ -5,86 +5,111 @@ import os
 import asyncio
 import aiohttp
 
-async def fetchIntradyPriceData(symbol: str, interval: str, month: str, api_key: str) -> pd.DataFrame:
-    print (f"Fetching intraday price data for {symbol} for the month of {month}...")
 
-    api_domain = "www.alphavantage.co"
-    function = "TIME_SERIES_INTRADAY"
-    output_size = "full"
+def createUrl(api_domain: str, function: str, api_key: str, params: dict) -> str:
+    url = f"https://{api_domain}/query?function={function}&apikey={api_key}"
 
-    url = "https://{}/query?function={}&symbol={}&interval={}&month={}&outputsize={}&apikey={}".format(
-        api_domain, function, symbol, interval, month, output_size, api_key)
+    for key, value in params.items():
+        url += f"&{key}={value}"
 
+    return url
+
+
+async def fetch_data(
+    url: str,
+    data_key: str,
+    column_mapping: dict
+) -> pd.DataFrame:
+    print(url)
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.json()
-            print("Data: ", data)
 
-            price_data = data.get("Time Series (1min)")
+            fetched_data = data.get(data_key)
 
-            if not price_data:
-                print(f"Error: {data.get('Information')}")
-                return
+            if not fetched_data:
+                raise Exception(f"Error: {data.get('Information')}")
 
             # create data frame
             data_frame = pd.DataFrame(
-                columns=["Date", "Open", "High", "Low", "Close", "Volume"])
+                columns=["Date"] + list(column_mapping.values()))
 
-            for date, price in price_data.items():
-                data_frame = pd.concat([data_frame, pd.DataFrame([{
-                    "Date": date,
-                    "Open": price['1. open'],
-                    "High": price['2. high'],
-                    "Low": price['3. low'],
-                    "Close": price['4. close'],
-                    "Volume": price['5. volume']
-                }])], ignore_index=True)
+            for date, item in fetched_data.items():
+                row = {"Date": date}
+                for column, key in column_mapping.items():
+                    row[column] = item[key]
+                data_frame = pd.concat(
+                    [data_frame, pd.DataFrame([row])], ignore_index=True)
 
             data_frame["Date"] = pd.to_datetime(data_frame["Date"])
 
             return data_frame
+
+
+async def fetchIntradyPriceData(symbol: str, interval: str, month: str, api_key: str) -> pd.DataFrame:
+    function = "TIME_SERIES_INTRADAY"
+    data_key = "Time Series ({})".format(interval)
+    column_mapping = {
+        "Open": "1. open",
+        "High": "2. high",
+        "Low": "3. low",
+        "Close": "4. close",
+        "Volume": "5. volume"
+    }
+
+    params = {
+        "apikey": api_key,
+        "function": function,
+        "month": month,
+        "interval": interval,
+        "symbol": symbol,
+        "outputsize": "full",
+
+    }
+
+    url = createUrl("www.alphavantage.co", function, api_key, params)
+
+    print(f"{month} Intraday Price - Fetching...")
+    data_frame = await fetch_data(url, data_key, column_mapping)
+    print(f"{month} Intraday Price - Done")
+
+    return data_frame
 
 
 async def fetchSMAData(symbol: str, interval: str, month: str, time_period: int, api_key: str) -> pd.DataFrame:
-    print(f"Fetching SMA data for {symbol} for the month of {month}...")
-
-    api_domain = "www.alphavantage.co"
     function = "SMA"
-    series_type = "close"
+    data_key = "Technical Analysis: SMA"
+    column_mapping = {
+        "SMA_{}".format(time_period): "SMA"
+    }
 
-    url = "https://{}/query?function={}&month={}&symbol={}&interval={}&time_period={}&series_type={}&apikey={}".format(
-        api_domain, function, month, symbol, interval, time_period, series_type, api_key)
+    params = {
+        "apikey": api_key,
+        "function": function,
+        "symbol": symbol,
+        "interval": interval,
+        "time_period": time_period,
+        "series_type": "close",
+        "month": month,
+    }
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
+    url = createUrl("www.alphavantage.co", function, api_key, params)
 
-            sma_data = data.get("Technical Analysis: SMA")
+    print(f"{month} SMA_{time_period} - Fetching...")
 
-            if not sma_data:
-                print(f"Error: {data.get('Information')}")
-                return
+    data_frame = await fetch_data(url, data_key, column_mapping)
 
-            # create data frame
-            data_frame = pd.DataFrame(columns=["Date", "SMA_{}".format(time_period)])
+    print(f"{month} SMA_{time_period} - Done")
 
-            for date, sma in sma_data.items():
-                data_frame = pd.concat([data_frame, pd.DataFrame([{
-                    "Date": date,
-                    "SMA_{}".format(time_period): sma['SMA']
-                }])], ignore_index=True)
-
-            data_frame["Date"] = pd.to_datetime(data_frame["Date"])
-
-            return data_frame
+    return data_frame
 
 
 async def createMergedCSV(symbol: str, interval: str, month: str, api_key: str, result_file: str):
     tasks = [
-        fetchIntradyPriceData(symbol, interval, month, api_key),
-        # fetchSMAData(symbol, interval, month, 10, api_key),
-        # fetchSMAData(symbol, interval, month, 50, api_key),
-        # fetchSMAData(symbol, interval, month, 100, api_key)
+        fetchIntradyPriceData(symbol, interval, month, api_key=api_key),
+        fetchSMAData(symbol, interval, month, 10, api_key=api_key),
+        fetchSMAData(symbol, interval, month, 50, api_key=api_key),
+        fetchSMAData(symbol, interval, month, 100, api_key=api_key)
     ]
 
     results = await asyncio.gather(*tasks)
@@ -96,7 +121,8 @@ async def createMergedCSV(symbol: str, interval: str, month: str, api_key: str, 
     df = df.merge(sma_50_data, how="outer", on="Date")
     df = df.merge(sma_100_data, how="outer", on="Date")
 
-    print(df.head())
+    # remove rows with missing values
+    df = df.dropna()
 
     if not os.path.exists(result_file):
         df.to_csv(result_file, mode='w', header=True, index=False)
