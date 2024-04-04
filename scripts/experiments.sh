1c5385bd7fba32e9a568e421413de07783b8cf33
Joseph Kim <clo3olb@gmail.com>
2024-04-04 17:33:15 +0000

    less tickers

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -1,35 +1,36 @@
 # model_name, data_name, input_size
 
+set -e
+
 
 # # Sin Wave
-# sh scripts/PatchTST.sh PatchTST_sin_wave sin_wave.csv 5
-# sh scripts/DLinear.sh DLinear_sin_wave sin_wave.csv 5
-# sh scripts/Transformer.sh Transformer_sin_wave sin_wave.csv 5
-# sh scripts/Autoformer.sh Autoformer_sin_wave sin_wave.csv 5
+# sh scripts/PatchTST.sh PatchTST_sin_wave sin_wave.csv 19
+# sh scripts/DLinear.sh DLinear_sin_wave sin_wave.csv 19
+# sh scripts/Transformer.sh Transformer_sin_wave sin_wave.csv 19
+# sh scripts/Autoformer.sh Autoformer_sin_wave sin_wave.csv 19
 
 # # AAPL
-# sh scripts/PatchTST.sh PatchTST_AAPL AAPL.csv 5
-# sh scripts/DLinear.sh DLinear_AAPL AAPL.csv 5
-# sh scripts/Autoformer.sh Autoformer_AAPL AAPL.csv 5
-# sh scripts/Transformer.sh Transformer_AAPL AAPL.csv 5
+# sh scripts/PatchTST.sh PatchTST_AAPL AAPL.csv 19
+# sh scripts/DLinear.sh DLinear_AAPL AAPL.csv 19
+# sh scripts/Autoformer.sh Autoformer_AAPL AAPL.csv 19
+# sh scripts/Transformer.sh Transformer_AAPL AAPL.csv 19
 
 
 # # # AAPL_pct
-# sh scripts/PatchTST.sh PatchTST_AAPL_pct AAPL_pct 5
-# sh scripts/DLinear.sh DLinear_AAPL_pct AAPL_pct 5
-# sh scripts/Autoformer.sh Autoformer_AAPL_pct AAPL_pct 5
-# sh scripts/Transformer.sh Transformer_AAPL_pct AAPL_pct 5
+# sh scripts/PatchTST.sh PatchTST_AAPL_pct AAPL_pct 19
+# sh scripts/DLinear.sh DLinear_AAPL_pct AAPL_pct 19
+# sh scripts/Autoformer.sh Autoformer_AAPL_pct AAPL_pct 19
+# sh scripts/Transformer.sh Transformer_AAPL_pct AAPL_pct 19
 
-symbols=("MSFT" "AMZN" "GOOGL" "NFLX"
-        "JPM" "BAC" "C" "WFC" "GS"\
-        "KO" "PG" "MCD" "DIS" "NKE"\
-        "JNJ" "PFE" "MRK" "ABT" "BMY"\
-        "XOM" "CVX" "COP" "SLB" "PSX")
+symbols=("JPM" "BAC" \
+        "KO" "PG" \
+        "JNJ" "PFE" \
+        "XOM" "CVX" )
 
 # other symbols
 for symbol in "${symbols[@]}"
 do
-    sh scripts/PatchTST.sh PatchTST_$symbol $symbol.csv 5
-    sh scripts/PatchTST.sh PatchTST_${symbol}_pct ${symbol}_pct.csv 5
+    sh scripts/PatchTST.sh PatchTST_$symbol $symbol.csv 19
+    sh scripts/PatchTST.sh PatchTST_${symbol}_pct ${symbol}_pct.csv 19
 
 done
