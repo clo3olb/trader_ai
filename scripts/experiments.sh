bccf5e77c7b202be2cecfeaf093a83e1a8b53fcb
Joseph Kim <clo3olb@gmail.com>
2024-04-05 02:47:07 +0000

    more experiments

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -2,28 +2,17 @@
 
 set -e
 
-
-# # Sin Wave
-# sh scripts/PatchTST.sh PatchTST_sin_wave sin_wave.csv 19
-# sh scripts/DLinear.sh DLinear_sin_wave sin_wave.csv 19
-# sh scripts/Transformer.sh Transformer_sin_wave sin_wave.csv 19
-# sh scripts/Autoformer.sh Autoformer_sin_wave sin_wave.csv 19
-
-# # AAPL
-# sh scripts/PatchTST.sh PatchTST_AAPL AAPL.csv 19
+# AAPL
 # sh scripts/DLinear.sh DLinear_AAPL AAPL.csv 19
-# sh scripts/Autoformer.sh Autoformer_AAPL AAPL.csv 19
-# sh scripts/Transformer.sh Transformer_AAPL AAPL.csv 19
+# sh scripts/Autoformer.sh Autoformer_AAPL AAPL.csv 18
+# sh scripts/Transformer.sh Transformer_AAPL AAPL.csv 18
 
+# # AAPL_pct
+# sh scripts/DLinear.sh DLinear_AAPL_pct AAPL_pct.csv 19
+sh scripts/Autoformer.sh Autoformer_AAPL_pct AAPL_pct.csv 18
+sh scripts/Transformer.sh Transformer_AAPL_pct AAPL_pct.csv 18
 
-# # # AAPL_pct
-# sh scripts/PatchTST.sh PatchTST_AAPL_pct AAPL_pct 19
-# sh scripts/DLinear.sh DLinear_AAPL_pct AAPL_pct 19
-# sh scripts/Autoformer.sh Autoformer_AAPL_pct AAPL_pct 19
-# sh scripts/Transformer.sh Transformer_AAPL_pct AAPL_pct 19
-
-symbols=("JPM" "BAC" \
-        "KO" "PG" \
+symbols=("PG" \
         "JNJ" "PFE" \
         "XOM" "CVX" )
 
@@ -31,6 +20,6 @@ symbols=("JPM" "BAC" \
 for symbol in "${symbols[@]}"
 do
     sh scripts/PatchTST.sh PatchTST_$symbol $symbol.csv 19
-    sh scripts/PatchTST.sh PatchTST_${symbol}_pct ${symbol}_pct.csv 19
+    # sh scripts/PatchTST.sh PatchTST_${symbol}_pct ${symbol}_pct.csv 19
 
 done
